287ad33d4021fd5e93f73212cfc9bce2eb473ed2
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-06-11 21:08:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="DiazBot-Prueba4"  
-BOT_REPO="https://github.com/KatashiFukushima/$BOT_DIR"
+BOT_REPO="https://github.com/diaz1975/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
